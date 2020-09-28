98be664faf385f4ecf998d77e3f99a18bf0fd016
yun-cheng <yuncheng.dev@gmail.com>
2020-09-28 18:32:40 +0800

    - 修改 unit.py     - Firestore interviewer Collection 改名成 interviewerList     - create method 加上 try/except

--- unit.py
+++ unit.py
@@ -7,40 +7,44 @@ class Unit:
         self.db = db
 
     def create(self, email):
+        try:
         # S_1 創立並設定新的 spreadsheet
-        # S_1-1 連接模板
-        template_gsid = '1VRGeK8m-w_ZCjg1SDQ74TZ7jpHsRiTiI3AcD54I5FC8'
-        template_spreadsheet = self.gsheets.open_by_key(template_gsid)
+            # S_1-1 連接模板
+            template_gsid = '1VRGeK8m-w_ZCjg1SDQ74TZ7jpHsRiTiI3AcD54I5FC8'
+            template_spreadsheet = self.gsheets.open_by_key(template_gsid)
 
-        # S_1-2 創立新的 spreadsheet
-        spreadsheet = self.gsheets.create('新建立之單位設定檔(可自訂名稱)')
-        gsid = spreadsheet.id
+            # S_1-2 創立新的 spreadsheet
+            spreadsheet = self.gsheets.create('新建立之單位設定檔(可自訂名稱)')
+            gsid = spreadsheet.id
 
-        # S_1-3 從模板複製到新創立的 spreadsheet
-        for i in range(3):
-            worksheet = template_spreadsheet.worksheet('index', i).copy_to(gsid)
-            worksheet.title = re.search(r'(?<=\s)\S+$', worksheet.title).group(0)
+            # S_1-3 從模板複製到新創立的 spreadsheet
+            for i in range(3):
+                worksheet = template_spreadsheet.worksheet('index', i).copy_to(gsid)
+                worksheet.title = re.search(r'(?<=\s)\S+$', worksheet.title).group(0)
 
-        # S_1-4 刪除初始 worksheet
-        sheet1 = spreadsheet.worksheet_by_title('Sheet1')
-        spreadsheet.del_worksheet(sheet1)
+            # S_1-4 刪除初始 worksheet
+            sheet1 = spreadsheet.worksheet_by_title('Sheet1')
+            spreadsheet.del_worksheet(sheet1)
 
-        # S_1-5 '更新此單位設定' 連結
-        worksheet = spreadsheet.worksheet_by_title('說明')
-        update_url = f'{main_url}?action=update&on=unit&gsid={gsid}'
-        worksheet.update_value('A3', f'=HYPERLINK("{update_url}", "更新此單位設定")')
+            # S_1-5 '更新此單位設定' 連結
+            worksheet = spreadsheet.worksheet_by_title('說明')
+            update_url = f'{main_url}?action=update&on=unit&gsid={gsid}'
+            worksheet.update_value('A3', f'=HYPERLINK("{update_url}", "更新此單位設定")')
 
-        # S_1-6 '刪除此單位' 連結
-        delete_url = f'{main_url}?action=delete&on=unit&gsid={gsid}'
-        worksheet.update_value('A4', f'=HYPERLINK("{delete_url}", "刪除此單位")')
+            # S_1-6 '刪除此單位' 連結
+            delete_url = f'{main_url}?action=delete&on=unit&gsid={gsid}'
+            worksheet.update_value('A4', f'=HYPERLINK("{delete_url}", "刪除此單位")')
 
-        # S_1-7 設定分享權限
-        email_message = '新建立之單位設定檔'
-        spreadsheet.share(email, 'writer', emailMessage=email_message)
-        # TODO 到時我的權限可拿掉
-        spreadsheet.share('yuncheng.dev@gmail.com', 'writer', emailMessage=email_message)
-        # NOTE 轉移所有權
-        # spreadsheet.share('yuncheng.dev@gmail.com', 'owner', transferOwnership=True)
+            # S_1-7 設定分享權限
+            email_message = '新建立之單位設定檔'
+            spreadsheet.share(email, 'writer', emailMessage=email_message)
+            # TODO 到時我的權限可拿掉
+            spreadsheet.share('yuncheng.dev@gmail.com', 'writer', emailMessage=email_message)
+            # NOTE 轉移所有權
+            # spreadsheet.share('yuncheng.dev@gmail.com', 'owner', transferOwnership=True)
+
+        except:
+            return '建立單位失敗!'
 
         return f'新建立之單位設定檔連結已寄至信箱（可能會在垃圾郵件中....），或複製此連結進入：<br/><br/> {spreadsheet.url}'
 
@@ -105,11 +109,11 @@ class Unit:
                 gsid: unit_info_dict
             }, merge=True)
 
-            # S_2-3 更新 Firestore: interviewer/{unitId}
+            # S_2-3 更新 Firestore: interviewerList/{unitId}
             # TAG Firestore SET
             # EXAMPLE
             '''
-            interviewer / {unitId} / {
+            interviewerList / {unitId} / {
                 {interviewerId}: {
                     interviewerId: 'id001',
                     interviewerPassword: 'password001',
@@ -117,13 +121,13 @@ class Unit:
                 }
             }
             '''
-            interviewer_df = get_worksheet_df(spreadsheet, worksheet_title='訪員帳號', end='C')
-            interviewer_dict = df_to_dict(interviewer_df,
-                                          new_column_names=['interviewerId', 'interviewerPassword', 'interviewerName'],
-                                          index_column='interviewerId')
+            interviewer_list_df = get_worksheet_df(spreadsheet, worksheet_title='訪員帳號', end='C')
+            interviewer_list_dict = df_to_dict(interviewer_list_df,
+                                               new_column_names=['interviewerId', 'interviewerPassword', 'interviewerName'],
+                                               index_column='interviewerId')
 
-            interviewer_ref = self.db.document('interviewer', gsid)
-            batch.set(interviewer_ref, interviewer_dict)
+            interviewer_list_ref = self.db.document('interviewerList', gsid)
+            batch.set(interviewer_list_ref, interviewer_list_dict)
 
             # S_2-4 更新 Firestore:
             # TODO 訪員有變化時，受影響的其他 collection
@@ -152,10 +156,10 @@ class Unit:
                 gsid: firestore.DELETE_FIELD
             })
 
-            # S_1-3 刪除 Firestore: interviewer/{unitId}
+            # S_1-3 刪除 Firestore: interviewerList/{unitId}
             # TAG Firestore DELETE
-            interviewer_ref = self.db.document('interviewer', gsid)
-            batch.delete(interviewer_ref)
+            interviewer_list_ref = self.db.document('interviewerList', gsid)
+            batch.delete(interviewer_list_ref)
 
             # S_1-4 刪除 interviewer_quiz/{interviewer_id_project_id}/{data}
             # TAG Firestore DELETE
