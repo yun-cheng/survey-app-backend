41f47694238c4fc4ce31787c56d43cd7e309f7f8
yun-cheng <yuncheng.dev@gmail.com>
2020-09-28 23:03:57 +0800

    簡化 app.py

--- app.py
+++ app.py
@@ -23,50 +23,29 @@ def main():
     gsheets = load_gsheets()
     db = load_firestore()
 
-    # H_3-c1 unit
+    # H_3 action
     if on == 'unit':
-        unit = Unit(gsheets=gsheets, db=db)
-
-        if action == 'create' and email:
-            return unit.create(email=email)
-
-        elif action == 'update' and gsid:
-            return unit.update(gsid=gsid)
-
-        elif action == 'delete' and gsid:
-            return unit.delete(gsid=gsid)
-
-    # H_3-c2 project
+        target = Unit(gsheets=gsheets, db=db)
     elif on == 'project':
-        project = Project(gsheets=gsheets, db=db)
-
-        if action == 'create' and email:
-            return project.create(email=email)
-
-        elif action == 'update' and gsid:
-            return project.update(gsid=gsid)
-
-        elif action == 'delete' and gsid:
-            return project.delete(gsid=gsid)
-
-    # H_3-c3 quiz
+        target = Project(gsheets=gsheets, db=db)
     elif on == 'quiz':
-        quiz = Quiz(gsheets=gsheets, db=db)
+        target = Quiz(gsheets=gsheets, db=db)
 
+    if on:
         if action == 'create' and email:
-            return quiz.create(email=email)
+            return target.create(email=email)
 
         elif action == 'update' and gsid:
-            return quiz.update(gsid=gsid)
-
-        elif action == 'update_result' and gsid and project_gsid and interviewer_id:
-            return quiz.update_result(gsid=gsid, project_gsid=project_gsid, interviewer_id=interviewer_id)
+            return target.update(gsid=gsid)
 
         elif action == 'delete' and gsid:
-            return quiz.delete(gsid=gsid)
+            return target.delete(gsid=gsid)
 
-        elif action == 'delete_result' and gsid:
-            return quiz.delete_result(gsid=gsid)
+        elif on == 'quiz' and action == 'update_result' and gsid and project_gsid and interviewer_id:
+            return target.update_result(gsid=gsid, project_gsid=project_gsid, interviewer_id=interviewer_id)
+
+        elif on == 'quiz' and action == 'delete_result' and gsid:
+            return target.delete_result(gsid=gsid)
 
     return '系統出現問題....'
 
